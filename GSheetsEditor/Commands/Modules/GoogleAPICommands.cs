7424856191854d1d54d4541befdbd419448cf2ad
Unchpokable <unchpokable.official@gmail.com>
2022-12-18 01:32:27 +0500

    Added Google Drive API service, implemented /export command

--- GSheetsEditor/Commands/Modules/GoogleAPICommands.cs
+++ GSheetsEditor/Commands/Modules/GoogleAPICommands.cs
@@ -7,17 +7,20 @@ using System.Text.RegularExpressions;
 using System.Text;
 using GSheetsEditor.Extensions;
 using Google.Apis.Sheets.v4.Data;
+using Google.Apis.Drive.v3;
+using Telegram.Bot.Types.InputFiles;
 
 namespace GSheetsEditor.Commands.Modules
 {
     [CommandModule]
     internal class GoogleAPICommands
     {
-        private static readonly string[] _scopes = { SheetsService.Scope.Spreadsheets };
+        private static readonly string[] _scopes = { SheetsService.Scope.Spreadsheets, SheetsService.Scope.Drive, DriveService.Scope.Drive };
         private static readonly string _applicationName = "app";
         private static string _defaultSpreadsheetID = "1wG0yquhycja9AWrf1xjvCbkDW5UA2BTPtQqeyKhzST0";
         private static readonly string _baseSheet = "Sheet1";
-        private static SheetsService _service;
+        private static SheetsService _sheetsService;
+        private static DriveService _driveService;
 
         static GoogleAPICommands()
         {
@@ -29,7 +32,13 @@ namespace GSheetsEditor.Commands.Modules
                                 .CreateScoped(_scopes);
             }
 
-            _service = new SheetsService(new BaseClientService.Initializer()
+            _sheetsService = new SheetsService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credentials,
+                ApplicationName = _applicationName,
+            });
+
+            _driveService = new DriveService(new BaseClientService.Initializer() 
             {
                 HttpClientInitializer = credentials,
                 ApplicationName = _applicationName,
@@ -51,14 +60,14 @@ namespace GSheetsEditor.Commands.Modules
 
             try
             {
-                var value = _service.Spreadsheets.Values.Get(_defaultSpreadsheetID, $"{_baseSheet}!{cell}").Execute().Values;
+                var value = _sheetsService.Spreadsheets.Values.Get(_defaultSpreadsheetID, $"{_baseSheet}!{cell}").Execute().Values;
                 if (value == null)
                     return new CommandExecutionResult("Empty Cell");
                 return new CommandExecutionResult(TableFetchedRangeToString(value, cell[0], int.Parse(cell.From(1).To(':'))));
             }
             catch (Exception ex)
             {
-                return new CommandExecutionResult($"ReadValue method exception thrown: {ex.Message}");
+                return new CommandExecutionResult($"Google API method exception thrown: {ex.Message}");
             }
         }
 
@@ -87,15 +96,54 @@ namespace GSheetsEditor.Commands.Modules
             valueRange.Values = new List<IList<object>> { valuesToWrite }; //TODO: Fix that it tries to write all data in single row
             try
             {
-                var appendRequest = _service.Spreadsheets.Values.Update(valueRange, _defaultSpreadsheetID, cell);
+                var appendRequest = _sheetsService.Spreadsheets.Values.Update(valueRange, _defaultSpreadsheetID, cell);
                 appendRequest.ValueInputOption = SpreadsheetsResource.ValuesResource.UpdateRequest.ValueInputOptionEnum.USERENTERED;
                 var appendResponce = appendRequest.Execute();
                 return new CommandExecutionResult($"Spreadsheet updated");
             }
             catch ( Exception ex )
             {
-                return new CommandExecutionResult($"ReadValue method exception thrown: {ex.Message}");
+                return new CommandExecutionResult($"Google API method exception thrown: {ex.Message}");
             }
+        }
+
+        [Command("/export")]
+        public static CommandExecutionResult ExportSpreadsheet(object parameter)
+        {
+            var request = _driveService.Files.List();
+            request.Q = "mimeType = 'application/vnd.google-apps.spreadsheet'";
+            var response = request.Execute();
+
+            if (response == null)
+                return new CommandExecutionResult("Drive Error: No such spreadsheet");
+
+            var requestedSpreadsheet = response.Files.FirstOrDefault(file => file.Id == _defaultSpreadsheetID);
+
+            if (requestedSpreadsheet == null)
+                return new CommandExecutionResult("Drive Error: No such spreadsheet");
+
+            var localPath = $"{AppContext.BaseDirectory}\\local-{_defaultSpreadsheetID}.xlsx";
+
+
+            using (var localFile = new FileStream(localPath, FileMode.Create, FileAccess.ReadWrite))
+            {
+                var file = _driveService.Files.Export(requestedSpreadsheet.Id, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
+
+                file.MediaDownloader.ProgressChanged += progress =>
+                {
+                    switch (progress.Status)
+                    {
+                        case Google.Apis.Download.DownloadStatus.Failed:
+                            Console.WriteLine($"[ERR] :: Download Failed: {progress.Exception}");
+                            break;
+                    }
+                };
+
+                file.Download(localFile);
+                var fileUri = new Uri(localPath, UriKind.Absolute);
+                return new CommandExecutionResult(fileUri);
+            }
+
         }
 
         private static bool ValidateCell(object parameter, out CommandExecutionResult errorMessage)
@@ -110,10 +158,10 @@ namespace GSheetsEditor.Commands.Modules
             string regexValidationMatch;
 
             if (maybeRange.Length == 1)
-                regexValidationMatch = @"[A-z][0-9]+";
+                regexValidationMatch = @"^[A-z][0-9]+$";
 
             else if (maybeRange.Length == 2)
-                regexValidationMatch = @"[A-z][0-9]+:[A-z][0-9]+";
+                regexValidationMatch = @"^[A-z][0-9]+:[A-z][0-9]+$";
 
             else
             {
