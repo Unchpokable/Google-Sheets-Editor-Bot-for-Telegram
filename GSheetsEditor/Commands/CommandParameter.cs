8fe57e36acf491086c4fdf67807aa1bfbc0cedb7
Unchpokable <unchpokable.official@gmail.com>
2022-12-20 19:48:08 +0500

    Updated command API

---
+++ GSheetsEditor/Commands/CommandParameter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GSheetsEditor.Commands
+{
+    internal class CommandParameter
+    {
+        public CommandParameter(long userID, object commandArgs)
+        {
+            UserID = userID;
+            CommandArgs = commandArgs;
+        }
+
+        public long UserID { get; init; }
+        public object CommandArgs { get; init; }
+    }
+}
